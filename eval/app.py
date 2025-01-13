9e9f483f620590f037bb730015e376308b89c06f
Neeraj319 <itskop520@gmail.com>
2025-01-13 02:50:39 +0000

    feat: run only one eval file

--- eval/app.py
+++ eval/app.py
@@ -1,5 +1,6 @@
 # pyright: reportPrivateImportUsage=false
 import asyncio
+import csv
 from enum import Enum
 from typing import Literal
 from appium import webdriver
@@ -11,6 +12,7 @@ from typer import Typer
 from appium.options.android import UiAutomator2Options
 from locatr import LlmSettings, LlmProvider, Locatr, LocatrAppiumSettings
 import os
+import typer
 
 
 app = Typer()
@@ -68,7 +70,15 @@ class EvalConfigYaml(BaseModel):
     device_name: str = Field(alias="deviceName")
 
 
-def parse_yaml(file_path: str) -> Union[EvalConfigYaml, None]:
+class EvalResult(BaseModel):
+    user_request: str
+    passed: bool
+    generated_locatrs: list[str]
+    expected_locatrs: list[str]
+    error: str | None = Field(default=None)
+
+
+def parse_yaml(file_path: str) -> EvalConfigYaml:
     with open(file_path, "r") as cf:
         loaded_yaml = yaml.safe_load(cf)
         return EvalConfigYaml.model_validate(loaded_yaml)
@@ -88,13 +98,14 @@ def start_appium_server(config: EvalConfigYaml) -> webdriver.Remote:
             language="en",
             locale="US",
         )
-    print(config.server_url)
     opts = UiAutomator2Options().load_capabilities(cap)
-    driver = webdriver.Remote("http://172.30.192.1:4723", options=opts)
+    driver = webdriver.Remote(str(config.server_url), options=opts)
     return driver
 
 
-def setup_locatr(appium_url: str, appium_session_id: str) -> Locatr:
+def setup_locatr(
+    appium_url: str, appium_session_id: str, cache_path: str, use_cache: bool
+) -> Locatr:
     llm_settings = LlmSettings(
         llm_provider=LlmProvider.OPENAI,
         llm_api_key=os.environ.get("LLM_API_KEY"),
@@ -105,37 +116,89 @@ def setup_locatr(appium_url: str, appium_session_id: str) -> Locatr:
         llm_settings=llm_settings,
         appium_url=appium_url,
         appium_session_id=appium_session_id,
+        cache_path=cache_path,
+        use_cache=use_cache,
     )
     return Locatr(locatr_settings_appium)
 
 
-async def run_eval(config: EvalConfigYaml):
-    driver = start_appium_server(config)
+async def run_eval(eval_config: EvalConfigYaml) -> List[EvalResult]:
+    results: List[EvalResult] = []
+    driver = start_appium_server(eval_config)
     locatr = setup_locatr(
-        appium_url="http://172.30.192.1:4723",
+        appium_url=str(eval_config.server_url),
         appium_session_id=driver.session_id,
+        cache_path=eval_config.config.cache_path,
+        use_cache=eval_config.config.use_cache,
     )
     try:
         selector = ""
-        for step in config.steps:
+        for step in eval_config.steps:
             if isinstance(step, LocateStep):
-                selector = locatr.get_locatr(step.user_request).selectors[0]
+                typer.echo(
+                    f"Locating element with user request: {step.user_request}"
+                )
+                try:
+                    selectors = locatr.get_locatr(step.user_request)
+                    selector = selectors.selectors[0]
+                    results.append(
+                        EvalResult(
+                            user_request=step.user_request,
+                            passed=True,
+                            generated_locatrs=selectors.selectors,
+                            expected_locatrs=step.expected_locatrs,
+                        )
+                    )
+                except Exception as e:
+                    results.append(
+                        EvalResult(
+                            user_request=step.user_request,
+                            passed=False,
+                            generated_locatrs=[],
+                            expected_locatrs=step.expected_locatrs,
+                            error=str(e),
+                        )
+                    )
+                    typer.echo(f"Exception when fetching locatr {e}", err=True)
+                    return results
             elif isinstance(step, ActionStep):
+                typer.echo(
+                    f"Performing action: {step.action} on selector: {selector}"
+                )
                 element = driver.find_element(By.XPATH, selector)
                 match step.action:
                     case EvalActions.CLICK:
                         element.click()
             if step.timeout:
+                typer.echo(f"Sleeping for {step.timeout} seconds.")
                 await asyncio.sleep(step.timeout)
-
+    except Exception as e:
+        typer.echo(f"Error during evaluation: {e}", err=True)
     finally:
+        typer.echo("Quitting the Appium driver.")
         driver.quit()
+        return results
+
+
+def write_resulsts_to_csv(file_name: str, results: List[EvalResult]):
+    field_names = list(EvalResult.model_json_schema()["properties"].keys())
+    with open(file_name, "w") as fp:
+        writer = csv.DictWriter(fp, fieldnames=field_names)
+        writer.writeheader()
+        for result in results:
+            writer.writerow(result.model_dump())
 
 
 @app.command()
-def main():
-    config = EvalConfigYaml.model_validate(parse_yaml("eval/appium/clock.yaml"))
-    asyncio.run(run_eval(config))
+def run_one(file_path: str):
+    typer.echo(f"Running evaluation for YAML file: {file_path}")
+    try:
+        config = parse_yaml(file_path)
+        results = asyncio.run(run_eval(config))
+        write_resulsts_to_csv(file_path.replace("yaml", "csv"), results)
+        typer.echo("Evaluation completed successfully.")
+    except Exception as e:
+        typer.echo(f"Error during run_one execution: {e}", err=True)
 
 
 if __name__ == "__main__":
